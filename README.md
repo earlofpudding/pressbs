e42b2c5cec52d4121e0ea766057cfa68311b9aa8
earlofpudding <rafaqali@gmail.com>
2020-08-20 22:42:21 -0600

    Rigged textblob scoring

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Sentiment analysis of press releases for public companies to develop hold/sell/b
 
 ## Steps
 1. Develop csv file with required information (press release page, company name, time period)
+2. Throw csv file into the main.py script
+
+## Evaluation Method
+Obtain polarity and sentiment for headline, and content and throw it into the below formula which applies a fixed factor rating for both properties
+
+
 
 
 ## Considerations and Variables
@@ -11,3 +17,7 @@ Sentiment analysis of press releases for public companies to develop hold/sell/b
 2. Marketcap/Size of Company
 3. Diversification of Company (Products, Services, etc).
 4. Geopolitical landscape (where company operates)
+
+## Improvements
+
+Develop polarity and sentiment for various paragraphs of article content and from there determine a method to evaluate which paragraph represents the article most thoroughly. (Will require modifying fetching script to not concatenate into a single string for `article_content`)
--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 from textblob import TextBlob
 import csv
+import glob, os
 
+print("Pick a file to use as input\n")
 
+fileList = glob.glob('data/*.csv')
 
+for i in range(len(fileList)):
+    print(str(i+1) + ": " + fileList[i])
+
+filePick = input("")
+while (int(filePick) not in range(len(fileList) + 1)):
+    filePick = input("")
+
+file = fileList[int(filePick) - 1]
+print("You picked: " + file)
+
+csv_headers = ['article_number', 'title_subjectivity', 'title_polarity', 'content_subjectivity', 'content_polarity', 'overall_score']
+
+with open(file, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row)
+
+        print(row['article_name'])
+        print("For article " + row['article_number'])
+        print("---------------------------")
+        print("Title: ")
+
+        title = TextBlob(row['article_name']).sentiment
+        print(title.polarity)
+        print(title.subjectivity)
+
+        content = TextBlob(row['article_content']).sentiment
+        print(content.polarity)
+        print(content.subjectivity)
