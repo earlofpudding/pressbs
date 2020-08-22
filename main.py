60230977c066217df60f0b12d54300edf13612b8
earlofpudding <rafaqali@gmail.com>
2020-08-22 11:11:08 -0600

    Analysis rigged to output to CSV file

--- main.py
+++ main.py
@@ -16,22 +16,36 @@ while (int(filePick) not in range(len(fileList) + 1)):
 file = fileList[int(filePick) - 1]
 print("You picked: " + file)
 
-csv_headers = ['article_number', 'title_subjectivity', 'title_polarity', 'content_subjectivity', 'content_polarity', 'overall_score']
+csv_headers = ['article_number', 'article_link', 'article_name', 'title_subjectivity', 'title_polarity', 'content_subjectivity', 'content_polarity', 'overall_score']
+csv_content = []
 
 with open(file, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row)
-
-        print(row['article_name'])
-        print("For article " + row['article_number'])
-        print("---------------------------")
-        print("Title: ")
-
         title = TextBlob(row['article_name']).sentiment
-        print(title.polarity)
-        print(title.subjectivity)
-
         content = TextBlob(row['article_content']).sentiment
-        print(content.polarity)
-        print(content.subjectivity)
+
+        csv_content.append({
+            "article_number": row['article_number'],
+            'article_link': row['article_link'],
+            'article_name': row['article_name'],
+            "title_subjectivity": title.subjectivity,
+            "title_polarity": title.polarity,
+            "content_subjectivity": content.subjectivity,
+            "content_polarity": content.polarity,
+            "overall_score": "N/A"
+        })
+        
+        # TODO include the weighed calculation of the subjectvity and polarity based on heading and contnet
+
+
+    outputFile = file.replace(".csv", "") + "-analysis.csv"
+    try:
+        with open(outputFile, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_headers)
+            writer.writeheader()
+            for data in csv_content:
+                writer.writerow(data)
+            print("Successfully created csv file: " + outputFile)
+    except IOError:
+        print("I/O error")
